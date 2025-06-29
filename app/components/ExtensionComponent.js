96427b2d6d5c41c3e68657918219276ccef60112
kofiakyea <kofiaakyea@gmail.com>
2025-06-29 17:26:16 +0000

    added some toggle logic

--- app/components/ExtensionComponent.js
+++ app/components/ExtensionComponent.js
@@ -1,26 +1,41 @@
 import React from "react";
 import Image from "next/image";
 
-const ExtensionComponent = ({ logo, name, description, isActive }) => {
+const ExtensionComponent = ({
+  logo,
+  name,
+  description,
+  isActive,
+  onToggle,
+}) => {
   return (
-    <div className="w-[100%] h-[200px] bg-white rounded-lg">
-      <div>
-        <div>
-          <Image src={logo} alt="extention logo" width={30} height={30} />
+    <div className="w-[100%] h-[200px] bg-white rounded-lg p-5 mt-5 shadow-lg flex flex-col justify-between">
+      <div className="flex">
+        <div className="w-[25%]">
+          <Image src={logo} alt="extention logo" width={60} height={60} />
         </div>
-        <div>
-          <p>{name}</p>
+        <div className="w-[65%]">
+          <h3 className="text-xl font-bold">{name}</h3>
           <p>{description}</p>
         </div>
       </div>
 
-      <div>
+      <div className="flex items-center justify-between">
         <button>Remove</button>
         <button
           aria-checked={isActive}
           role="switch"
-          className={`block w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300`}
-        ></button>
+          onClick={() => onToggle(name)}
+          className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors duration-300 focus:outline-none ${
+            isActive ? "bg-blue-600" : "bg-gray-200"
+          }`}
+        >
+          <span
+            className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-300 ${
+              isActive ? "translate-x-6" : "translate-x-1"
+            }`}
+          />
+        </button>
       </div>
     </div>
   );
--- app/components/ExtensionsList.js
+++ app/components/ExtensionsList.js
@@ -1,16 +1,32 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import ExtensionComponent from "./ExtensionComponent";
 import data from "@/app/data.json";
 
 const ExtensionsList = () => {
+  const [extension, setExtensions] = useState(data);
+
+  const handleToggle = (name) => {
+    setExtensions((prevExtensions) =>
+      prevExtensions.map((extension) =>
+        extension.name === name
+          ? { ...extension, isActive: !extension.isActive }
+          : extension
+      )
+    );
+  };
+
   return (
-    <div>
+    <div className="mt-10">
       {data.map((item) => (
         <ExtensionComponent
-          key={item}
+          key={item.name}
           logo={item.logo}
           name={item.name}
           description={item.description}
+          isActive={item.isActive}
+          onToggle={handleToggle}
         />
       ))}
     </div>
